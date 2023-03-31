645e3de6565de7b20f6b26735844ab438691d0fd
Timur <timur.hike@gmail.com>
2023-03-31 12:47:16 +0300

    Add model Employee

--- create_hierarchy/models.py
+++ create_hierarchy/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    class PositionChoices(models.TextChoices):
+        GOD = "god"
+        SEO = "seo"
+        LEAD = "lead"
+        SENIOR = "senior"
+        MIDDLE = "middle"
+        JUNIOR = "junior"
+        TRAINEE = "trainee"
+
+    name = models.CharField(max_length=255)
+    position = models.CharField(max_length=255, choices=PositionChoices.choices)
+    hire_date = models.DateField()
+    email = models.EmailField()
+    manager = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    def __str__(self):
+        return self.name
